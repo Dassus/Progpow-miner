a109c2bf5b3a2dcc6fc402c99b5c3aada957f7cf
ifdefelse <ifdefelse@protonmail.com>
2019-01-18 19:09:44 -0800

    Update default work sizes
    ProgPoW depends on the localWorkSize/blockSize being as large as possible.  This enables the most threads to share the cached data stored in LDS/shared, significantly improving performance.
    
    ProgPoW on an RX580 goes from 7.2Mh/s -> 9.8 Mh/s.
    
    Makes no difference for ethash.

--- libethcore/Miner.h
+++ libethcore/Miner.h
@@ -107,8 +107,8 @@ struct CUSettings
     vector<unsigned> devices;
     unsigned streams = 2;
     unsigned schedule = 4;
-    unsigned gridSize = 8192;
-    unsigned blockSize = 128;
+    unsigned gridSize = 2048;
+    unsigned blockSize = 512;
     unsigned parallelHash = 4;
 };
 
@@ -118,8 +118,8 @@ struct CLSettings
     vector<unsigned> devices;
     bool noBinary = false;
     unsigned globalWorkSize = 0;
-    unsigned globalWorkSizeMultiplier = 65536;
-    unsigned localWorkSize = 128;
+    unsigned globalWorkSizeMultiplier = 32768;
+    unsigned localWorkSize = 256;
 };
 
 // Holds settings for CPU Miner
